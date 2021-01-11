c9cd266b2f5d9d65f79fc6dd76bc980f44d7a97c
Shashank <shashankshrestha84@gmail.com>
2021-01-11 21:40:36 +0545

    Database and Student class

--- app/src/main/java/com/example/week6_assignment1/model/Database.kt
+++ app/src/main/java/com/example/week6_assignment1/model/Database.kt
@@ -1,2 +1,17 @@
+package com.example.week6_assignment1.model
 
+val listStudents = mutableListOf(Student(0,"",0,"","",""))
+var loggedIn = listStudents[0]
+
+class Database {
+
+    fun appendStudent(student:Student){
+        listStudents.add(student)
+    }
+
+    fun returnStudent():MutableList<Student>{
+        return listStudents
+    }
+
+}
 
